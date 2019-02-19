2014b79c4a661615d6e53f2c13b11cb334899e87
joao <joao182cx@gmail.com>
2019-02-19 13:51:03 -0300

    Modifição do código do exercício anterior para que sejam contadas as ocorrências de cada caractere na frase.

--- src/Pesquisa.java
+++ src/Pesquisa.java
@@ -5,27 +5,45 @@ public class Pesquisa {
 	public static void main(String[] args) {
 		
 		String frase;
-		char caractere;
-		int quantidade = 0;
+		char[] alfabeto = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+				'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z', 'Y', 'W'};
+		
+		int[] ocorrencias = new int[alfabeto.length];
+		
 		int posicao = 0;
 		
 		Scanner input = new Scanner(System.in);	
 		
 		System.out.println("Insira uma frase:");
 		frase = input.nextLine();
+		frase = frase.toUpperCase();
 		
-		System.out.println("Insira um caractere para pesquisar na frase:");
-		caractere = input.next().charAt(0);
-		
-		while(frase.indexOf(caractere, posicao) >= 0) {
+		for(int i = 0; i < alfabeto.length; i++) {
 			
-			quantidade++;
-			posicao = frase.indexOf(caractere, posicao) + 1;
+			posicao = 0;
+			
+			while(frase.indexOf(alfabeto[i], posicao) >= 0) {
 				
+				ocorrencias[i]++;
+				posicao = frase.indexOf(alfabeto[i], posicao) + 1;
+					
+			}
+			
 		}
 		
-		System.out.printf("Ocorrências de \'%c\' em \"%s\": %d", caractere, frase, quantidade);
+		System.out.println("Resultado:");
+		
+		System.out.printf("Letra\tOcorrências na frase%n");
+		
+		for(int i = 0; i < alfabeto.length; i++) {
+			
+			System.out.printf("%c\t%d%n", alfabeto[i], ocorrencias[i]);
+			
+		}
+		
+		
 		
 	}
 
 }
+
